9410fcf5c22cb57f2cd82feb133e7f1462162fb4
talindo444 <matheusquiroz446@gmail.com>
2025-12-09 16:31:22 -0300

    Update maintenanceConfig.js

--- src/lib/maintenanceConfig.js
+++ src/lib/maintenanceConfig.js
@@ -1,6 +1,6 @@
 // Toggle de manutenção por código
 // Defina como true e faça deploy para ativar manutenção globalmente
-export const FORCE_MAINTENANCE = true;
+export const FORCE_MAINTENANCE = false;
 export const DEFAULT_MAINTENANCE_END = '';
 export const DEFAULT_MAINTENANCE_TEXT = 'Estamos em manutenção no momento. Voltaremos em breve.';
 export const BYPASS_PASSWORD = 'f740028922';
